616d6e34430c20ed8e222844becaf0f744f63f02
VSG\Pavlina <pveleva@vsgbg.com>
2021-11-11 12:56:54 +0200

    Creation of persistence layer through migrations

--- src/Infrastructure/EvaluationSystem.Persistence/Migrations/CreateQuestionData.cs
+++ src/Infrastructure/EvaluationSystem.Persistence/Migrations/CreateQuestionData.cs
@@ -10,11 +10,11 @@ namespace EvaluationSystem.Persistence.Migrations
     {
         public override void Up()
         {
-            Insert.IntoTable("QuestionTemplate").Row(new { Name = "Whom would you like to evaluate?", Type = 1, IsReusable = 1});
-            Insert.IntoTable("QuestionTemplate").Row(new { Name = "How likely is it that you would recommend your coworker to a colleague?", Type = 2, IsReusable = 1});
-            Insert.IntoTable("QuestionTemplate").Row(new { Name = "How often is your coworker late to work?", Type = 3, IsReusable = 1});
-            Insert.IntoTable("QuestionTemplate").Row(new { Name = "In a typical week are the hours clocked by your coworker greater than, fewer than, or about the same as the hours actually worked?", Type = 1, IsReusable = 1});
-            Insert.IntoTable("QuestionTemplate").Row(new { Name = "How much attention to detail does your coworker have?", Type = 4, IsReusable = 1});
+            Insert.IntoTable("QuestionTemplate").Row(new { Name = "Whom would you like to evaluate?", Type = 1, IsReusable = true });
+            Insert.IntoTable("QuestionTemplate").Row(new { Name = "How likely is it that you would recommend your coworker to a colleague?", Type = 2, IsReusable = true });
+            Insert.IntoTable("QuestionTemplate").Row(new { Name = "How often is your coworker late to work?", Type = 3, IsReusable = true });
+            Insert.IntoTable("QuestionTemplate").Row(new { Name = "In a typical week are the hours clocked by your coworker greater than, fewer than, or about the same as the hours actually worked?", Type = 1, IsReusable = true });
+            Insert.IntoTable("QuestionTemplate").Row(new { Name = "How much attention to detail does your coworker have?", Type = 4, IsReusable = true });
 
         }
         public override void Down()
