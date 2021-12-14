c276e36d6f6aaa7b6526cbce12cb53caa2ae17c9
VSG\Pavlina <pveleva@vsgbg.com>
2021-12-14 12:12:27 +0200

    Update AnswerDto bool to byte

--- src/Core/EvaluationSystem.Application/Models/Answers/AnswerDto.cs
+++ src/Core/EvaluationSystem.Application/Models/Answers/AnswerDto.cs
@@ -4,7 +4,7 @@
     {
         public int IdQuestion { get; set; }
         public int Id { get; set; }
-        public byte IsDefault { get; set; }
+        public byte IsDefault { get; set; } = 0;
         public int Position { get; set; } = 1;
         public string AnswerText { get; set; }
         public byte IsAnswered { get; set; } = 0;
