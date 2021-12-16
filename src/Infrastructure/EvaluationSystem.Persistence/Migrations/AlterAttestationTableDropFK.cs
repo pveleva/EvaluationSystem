d96fdd1ea42997dbd851415764e2f8d020ad3885
VSG\Pavlina <pveleva@vsgbg.com>
2021-12-16 08:56:40 +0200

    AlterAttestationTableDropFK migration unabled because of error

--- src/Infrastructure/EvaluationSystem.Persistence/Migrations/AlterAttestationTableDropFK.cs
+++ src/Infrastructure/EvaluationSystem.Persistence/Migrations/AlterAttestationTableDropFK.cs
@@ -2,7 +2,7 @@
 
 namespace EvaluationSystem.Persistence.Migrations
 {
-    [Migration(202112151719)]
+    //[Migration(202112151719)]
     public class AlterAttestationTableDropFK : Migration
     {
         public override void Up()
