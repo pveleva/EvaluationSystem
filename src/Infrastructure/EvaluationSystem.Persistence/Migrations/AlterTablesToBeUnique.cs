3bdc9990380321e02d1e49853a672c1930dea6f8
VSG\Pavlina <pveleva@vsgbg.com>
2022-01-04 10:11:15 +0200

    AlterTablesNamesToBeUnique

--- src/Infrastructure/EvaluationSystem.Persistence/Migrations/AlterTablesToBeUnique.cs
+++ src/Infrastructure/EvaluationSystem.Persistence/Migrations/AlterTablesToBeUnique.cs
@@ -7,9 +7,6 @@ namespace EvaluationSystem.Persistence.Migrations
     {
         public override void Up()
         {
-            Alter.Table("AnswerTemplate")
-                .AlterColumn("AnswerText").AsString(255).Unique().NotNullable();
-
             Alter.Table("QuestionTemplate")
                 .AlterColumn("[Name]").AsString(1024).Unique().NotNullable();
 
@@ -18,9 +15,6 @@ namespace EvaluationSystem.Persistence.Migrations
         }
         public override void Down()
         {
-            Alter.Table("AnswerTemplate")
-                .AlterColumn("AnswerText").AsString(255).NotNullable();
-
             Alter.Table("QuestionTemplate")
                 .AlterColumn("[Name]").AsString(1024).NotNullable();
 
