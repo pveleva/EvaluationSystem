101fef5e6a6c268581be77a1030ebd6cb74de91c
VSG\Pavlina <pveleva@vsgbg.com>
2022-01-06 16:13:14 +0200

    AlterAttestationTableToDeleteCascade

--- src/Infrastructure/EvaluationSystem.Persistence/Migrations/AlterAttestationTableToDeleteCascade.cs
+++ src/Infrastructure/EvaluationSystem.Persistence/Migrations/AlterAttestationTableToDeleteCascade.cs
@@ -11,12 +11,12 @@ namespace EvaluationSystem.Persistence.Migrations
             Execute.Sql("ALTER TABLE Attestation DROP CONSTRAINT FK_Attestation_IdUserToEvaluate_User_Id;");
 
             Execute.Sql("ALTER TABLE Attestation ADD CONSTRAINT FK_Attestation_IdForm_AttestationForm_Id FOREIGN KEY(IdForm) REFERENCES AttestationForm(Id) ON DELETE CASCADE;");
-            Execute.Sql("ALTER TABLE Attestation ADD CONSTRAINT FK_Attestation_IdUserToEvaluate_User_Id FOREIGN KEY(IdUserToEvaluate) REFERENCES User(Id) ON DELETE CASCADE;");
+            Execute.Sql("ALTER TABLE Attestation ADD CONSTRAINT FK_Attestation_IdUserToEvaluate_User_Id FOREIGN KEY(IdUserToEvaluate) REFERENCES [User](Id) ON DELETE CASCADE;");
         }
         public override void Down()
         {
             Execute.Sql("ALTER TABLE Attestation ADD CONSTRAINT FK_Attestation_IdForm_AttestationForm_Id FOREIGN KEY(IdForm) REFERENCES AttestationForm(Id);");
-            Execute.Sql("ALTER TABLE Attestation ADD CONSTRAINT FK_Attestation_IdUserToEvaluate_User_Id FOREIGN KEY(IdUserToEvaluate) REFERENCES User(Id);");
+            Execute.Sql("ALTER TABLE Attestation ADD CONSTRAINT FK_Attestation_IdUserToEvaluate_User_Id FOREIGN KEY(IdUserToEvaluate) REFERENCES [User](Id);");
 
             Execute.Sql("ALTER TABLE Attestation DROP CONSTRAINT FK_Attestation_IdForm_AttestationForm_Id;");
             Execute.Sql("ALTER TABLE Attestation DROP CONSTRAINT FK_Attestation_IdUserToEvaluate_User_Id;");
