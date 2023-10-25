a3724de289c3615667daa3f12a97d5dad977694f
pavlinav <pavlina.velevagit@gmail.com>
2023-10-25 16:49:32 +0300

    implement Docker support

--- src/Infrastructure/EvaluationSystem.Persistence/CreateDatabase.cs
+++ src/Infrastructure/EvaluationSystem.Persistence/CreateDatabase.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace EvaluationSystem.Persistence.Migrations
 {
@@ -11,6 +12,13 @@ namespace EvaluationSystem.Persistence.Migrations
         {
             var masterConnectionString = configuration.GetConnectionString("MasterDBConnection");
             var evaluationConnectionString = new SqlConnectionStringBuilder(configuration.GetConnectionString("EvaluationSystemDBConnection"));
+            if (!String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DB_HOST")))
+            {
+                var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+                var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+                var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+                evaluationConnectionString = new SqlConnectionStringBuilder($"Data Source={dbHost};Initial Catalog={dbName};Integrated Security=True; MultipleActiveResultSets=True; Password={dbPassword}");
+            }
 
             var name = evaluationConnectionString.InitialCatalog;
 
